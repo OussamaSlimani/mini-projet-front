ae0505673a68d2c3ac6f01a39a4fb25cb2621385
Oussama Slimani <slimani.oussama@enis.tn>
2025-08-26 22:48:23 +0100

    added crud functions

---
+++ src/app/pages/models/account.model.ts
@@ -0,0 +1,11 @@
+export interface Account {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  title: string;
+  ms: string;
+  bm: string;
+  at: string;
+}
